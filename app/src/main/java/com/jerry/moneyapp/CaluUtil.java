34be6851538f1837a18049809f654b083729509b
Jerry <wzl-pc@JerryMac.local>
2018-10-08 19:35:25 +0800

    bug

--- app/src/main/java/com/jerry/moneyapp/CaluUtil.java
+++ app/src/main/java/com/jerry/moneyapp/CaluUtil.java
@@ -51,15 +51,20 @@ class CaluUtil {
                 point.multiple3 = -1;
             }
             if (!good) {
-                if (paint.size() > 1 && paint.get(0) == 1 && paint.get(1) > 1) {
-                    point.multiple2 = 2;
-                } else if (paint.size() == 1 && paint.get(0) == 1 && ints[position - 1] == GBData.VALUE_FENG) {
-                    point.multiple2 = 2;
+                int paintSize = paint.size();
+                if (paintSize > 1 && paint.get(0) == 1) {
+                    if (paintSize > 2 && paint.get(1) > 1) {
+                        point.multiple2 = 2;
+                    } else if (paintSize == 2) {
+                        point.multiple2 = 2;
+                    }
                 }
-                if (paint.size() > 2 && paint.get(0) == 1 && paint.get(1) == 1 && paint.get(2) > 1) {
-                    point.multiple3 = 2;
-                } else if (paint.size() == 2 && paint.get(0) == 1 && paint.get(1) == 1 && ints[position - 2] == GBData.VALUE_FENG) {
-                    point.multiple3 = 2;
+                if (paintSize > 2 && paint.get(0) == 1 && paint.get(1) == 1) {
+                    if (paintSize > 3 && paint.get(2) > 1) {
+                        point.multiple3 = 2;
+                    } else if (paintSize == 3) {
+                        point.multiple3 = 2;
+                    }
                 }
             }
             // 记录当前数到第几个
