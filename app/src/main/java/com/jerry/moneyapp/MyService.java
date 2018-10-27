792b014b1fd25b2013f87f773b4e17b8a26941ec
Jerry <wzl-pc@JerryMac.local>
2018-10-27 09:07:51 +0800

    参数调整

--- app/src/main/java/com/jerry/moneyapp/MyService.java
+++ app/src/main/java/com/jerry/moneyapp/MyService.java
@@ -137,30 +137,30 @@ public class MyService extends Service {
                     } else {
                         point.win3 = last.win3;
                     }
-                    if (mPoints.size() >= 15) {
-                        point.award2 = point.win2 - mPoints.get(mPoints.size() - 15).win2;
-                        point.award3 = point.win3 - mPoints.get(mPoints.size() - 15).win3;
+                    if (mPoints.size() > 6) {
+                        point.award2 = point.win2 - mPoints.get(6).win2;
+                        point.award3 = point.win3 - mPoints.get(6).win3;
                     } else {
                         point.award2 = point.win2;
                         point.award3 = point.win3;
+                    }
+                    if (last.intention != GBData.VALUE_NONE) {
+                        int mutiple;
+                        if (currentType == 2) {
+                            mutiple = last.multiple2;
+                        } else {
+                            mutiple = last.multiple3;
+                        }
+                        if (last.intention == ints[ints.length - 1]) {
+                            win += 9.7 * Math.abs(mutiple);
+                        } else {
+                            win -= 10 * Math.abs(mutiple);
+                        }
                     }
                 }
                 Log.d("win2", ints.length - 1 + "：" + String.valueOf(point.win2));
                 Log.d("win3", ints.length - 1 + "：" + String.valueOf(point.win3));
                 mPoints.add(point);
-            }
-            if (last.intention != GBData.VALUE_NONE) {
-                int mutiple;
-                if (currentType == 2) {
-                    mutiple = last.multiple2;
-                } else {
-                    mutiple = last.multiple3;
-                }
-                if (last.intention == ints[ints.length - 1]) {
-                    win += 9.7 * Math.abs(mutiple);
-                } else {
-                    win -= 10 * Math.abs(mutiple);
-                }
             }
             last = mPoints.get(mPoints.size() - 1);
             if (last.award2 >= last.award3) {
@@ -168,8 +168,8 @@ public class MyService extends Service {
             } else {
                 currentType = 3;
             }
-            double qiwang = -0.15 * ints.length;
-            if (ints.length > 6 && last.award2 > 0 && last.award3 > 0 && last.win2 > qiwang && last.win3 > qiwang) {
+            double qiwang = ints.length * 0.8;
+            if (ints.length > 6 && last.award2 >= -10 && last.award3 >= -10 && last.win2 > qiwang && last.win3 > qiwang) {
                 if (currentType == 2 && last.intention2 != GBData.VALUE_NONE) {
                     last.intention = last.intention2;
                     showJingsheng((getIntentStr(last.intention2, last.multiple2)));
